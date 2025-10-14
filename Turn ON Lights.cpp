b117ff010fef10239b1247a5dd1452db9d40ccea
Ryufath Alief Adhyaksa Putera Soepeno <89006072+RyufathSoepeno@users.noreply.github.com>
2025-10-14 11:55:29 +0700

    Create Turn ON Lights.cpp

---
+++ Turn ON Lights.cpp
@@ -0,0 +1,24 @@
+byte ldr = A2;
+byte led = 13;
+int nilai;
+
+
+void setup() {
+  pinMode(led, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  nilai = analogRead(ldr);
+  Serial.print("Nilai LDR: ");
+  Serial.println(nilai);
+
+  if (nilai < 100){
+    digitalWrite(led, HIGH);
+  }
+  else{
+    digitalWrite(led, LOW);
+  }
+
+}
